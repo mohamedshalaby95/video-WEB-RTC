a3b6f56369efdba9ce28375373cdb93921abb4d7
mohamed shalaby <mohamedshalaby19595@gmail.com>
2021-09-25 21:08:28 +0200

    instal cors

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+App.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}))
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
